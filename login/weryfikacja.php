7dd8b455f5248d214e247bd21966f660845da136
dawid ozdoba <121258641+pozytywnyozdobka@users.noreply.github.com>
2023-01-03 10:20:59 +0100

    Update weryfikacja.php

--- login/weryfikacja.php
+++ login/weryfikacja.php
@@ -1,18 +1,22 @@
 <?php
 $password = password_hash($_POST["haselko"], PASSWORD_DEFAULT);
-$con = mysqli_connect("localhost", "root", "", "real_chat");  
 
-$query = "INSERT INTO 'login' VALUES(email ";
+$img = $_POST["imgprof"];
+$con = mysqli_connect("localhost", "root", "", "real_chat");  
+$target_dir = "upload/";
+	$file = $_FILES['my_file']['name'];
+	$path = pathinfo($file);
+	$filename = $path['filename'];
+	$ext = $path['extension'];
+	$temp_name = $_FILES['my_file']['tmp_name'];
+	$path_filename_ext = $target_dir.$filename.".".$ext;
+$query = "INSERT INTO 'login' (email, haslo) VALUES('".."')";
 if(mysqli_connect_errno()) {  
     die("Could not connect: ". mysqli_connect_error());  
 }  
 $result = mysqli_query($con, $query); 
 $count = mysqli_num_rows($result);   
-if($count == 1){  
-    echo "<h1><center> Zalogowano pomyślnie! </center></h1>";  
-    header("Location: index.php");
-    exit();
-}     
+
 
 
 
